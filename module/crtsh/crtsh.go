74c203488ee600f1c3ce46e843b101a3d168060b
mzzsml <mzzsml@fastmail.co>
2023-10-04 17:34:05 +0200

    add: remove duplicate elements in crt.sh results

--- module/crtsh/crtsh.go
+++ module/crtsh/crtsh.go
@@ -8,24 +8,72 @@ import (
 )
 
 func QueryCrtsh(w http.ResponseWriter, h *http.Request, d string) {
+	var domainNames []string
+	var m map[string]interface{}
+
+	// Make the request to crt.sh, specifying the output type JSON
 	resp, err := http.Get("https://crt.sh/?q=" + d + "&output=json")
 	if err != nil {
 		panic(err)
 	}
 
+	// resp.Body is byte
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
+	// Create an interface that will hold JSON structure
+	// Unmarshal converts the JSON into the interface
 	var f []interface{}
 	err = json.Unmarshal(body, &f)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, item := range f {
-		m := item.(map[string]interface{})
-		fmt.Fprintln(w, m["common_name"].(string))
+	// Since the JSON returned is an array, we need to iterate through all JSONs
+	domainNames = make([]string, len(f))
+	for i, item := range f {
+		m = item.(map[string]interface{})
+		domainNames[i] = m["common_name"].(string)
 	}
+
+	var tempDomains = make([]string, 0, len(domainNames))
+	tempDomains = append(tempDomains, domainNames[0])
+	//j := 0
+	for _, d := range domainNames {
+		for i := 0; i < len(tempDomains); i++ {
+			if !isFound(d, tempDomains) {
+				tempDomains = append(tempDomains, d)
+			}
+			//if d != tempDomains[i] {
+			//	tempDomains = append(tempDomains, d)
+			//}
+		}
+		//for j < len(tempDomains) {
+		//	if d != tempDomains[j] {
+		//		tempDomains[j] = d
+		//	}
+		//	j++
+		//}
+	}
+
+	for _, d := range tempDomains {
+		fmt.Fprintln(w, d)
+	}
+	//for _, d := range domainNames {
+	//	fmt.Fprintln(w, d)
+	//}
+}
+
+func isFound(target string, slice []string) bool {
+	var found bool = false
+
+	for _, item := range slice {
+		if target == item {
+			found = true
+		}
+	}
+
+	return found
 }
