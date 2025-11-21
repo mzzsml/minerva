a8dfe4c4bee3a36a64e44b80188d720451341001
mzzsml <mzzsml@fastmail.com>
2025-11-21 21:32:55 +0100

    add: more test cases.

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -35,3 +35,69 @@ func TestMkdir(t *testing.T) {
         t.Errorf("Mkdir did not create test directory (%s), got error %v", testDir, err)
     }
 }
+
+//// Testare creazione directory duplicata.
+//func TestMkdirDuplicate(t *testing.T) {
+//    testDir := "/tmp/minerva-duplication-test"
+//
+//    err := Mkdir(testDir)
+//    if err != nil {
+//        t.Errorf("TestMkdirDuplicate: Mkdir did not create test directory (%s), got error %v", testDir, err)
+//    }
+//
+//    // Now let's try to create the same directory a second time.
+//    // If Mkdir() does not throw an error, this test is considered a fail.
+//    err = Mkdir(testDir)
+//    if err == nil {
+//        t.Errorf("TestMkdirDuplicate: Mkdir did not thew error on folder duplication (%s)", testDir)
+//    }
+//}
+
+func TestWriteToFile(t *testing.T) {
+    var c Config
+
+    c = Config{
+        ListenAddress: "asd",
+        DataSourceName: "qwe",
+    }
+
+    err := c.WriteToFile("/tmp/test-minerva/test-file-write")
+
+    if err != nil {
+        t.Errorf("TestWriteToFile: could not create te file: %s", err)
+    }
+}
+
+func TestLoadFromFile (t *testing.T) {
+    var c Config
+    
+    err := c.LoadFromFile("/tmp/test-minerva/test-file-read.toml")
+
+    if err != nil {
+        t.Errorf("TestLoadFromFile: error in reading file (%s) (%s)", "/tmp/test-minerva/test-file-read.toml", err)
+    }
+}
+
+func TestLoadFromFileNonExisting (t *testing.T) {
+    var c Config
+    
+    err := c.LoadFromFile("/tmp/test-minerva/test-file-read-non-existing.toml")
+
+    if err == nil {
+        t.Errorf("TestLoadFromFile: error is nil, and it shouldn't (%s)", err)
+    }
+}
+
+func TestLoadFromFileEmpty (t *testing.T) {
+    var c Config
+    
+    err := c.LoadFromFile("/tmp/test-minerva/test-file-read-empty.toml")
+
+    if err == nil {
+        t.Errorf("error is nil, and it shouldn't (%s)", err)
+    }
+}
+
+func TestFindConfigFile(t *testing.T) {}
+
+func TestCreateConfigFile(t *testing.T) {}
