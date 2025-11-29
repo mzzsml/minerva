93d314c23fe061b5b236026b18d89e3522bdb520
mzzsml <mzzsml@fastmail.com>
2025-11-29 21:25:15 +0100

    fix: undo removal of some functions.

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
     "os"
+    "path/filepath"
 
     "github.com/BurntSushi/toml"
 )
@@ -9,6 +10,15 @@ import (
 type Config struct {
     ListenAddress  string `toml:"ListenAddress"`
     DataSourceName string `toml:"DataSourceName"`
+}
+
+func ReturnConfigFilePath() (string, error) {
+    xdgDir, err := os.UserConfigDir()
+    if err != nil {
+        return "", err
+    }
+    configFilePath := filepath.Join(xdgDir, "minerva", "minerva.toml")
+    return configFilePath, err
 }
 
 // LoadFromFile reads the confiuration parameters from the given
@@ -31,4 +41,23 @@ func (c *Config) LoadFromFile(filename string) error {
     }
 
     return nil
+}
+
+
+// FindConfigFile simply looks for Minerva configuration files in
+// $XDG_CONFIG_HOME and &HOME/.config.
+// If none are found it returns a ErrNotExists.
+func FindConfigFile() (configFilePath string, err error) {
+    configFilePath, err = ReturnConfigFilePath()
+    if err != nil {
+        return "", err
+    }
+
+    _, err = os.Stat(configFilePath)
+    // If the file does not exists, return a ErrNotExist error.
+    if err != nil && os.IsNotExist(err) {
+        return "", err
+    }
+    // Otherwise, if the file exists, fileInfo will be populated.
+    return configFilePath, err
 }
