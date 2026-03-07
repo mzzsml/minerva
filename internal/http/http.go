25d0696b5ed7ede7bf69507aa281693af9be8925
mzzsml <mzzsml@fastmail.com>
2026-03-07 20:59:16 +0100

    add: delete host function

--- internal/http/http.go
+++ internal/http/http.go
@@ -1,121 +1,130 @@
 package http
 
 import (
-	"encoding/json"
-	"fmt"
-	"html/template"
-	"io"
-	"log"
-	"mime"
-	"mime/multipart"
-	"net/http"
-	"strings"
+    "encoding/json"
+    "fmt"
+    "html/template"
+    "io"
+    "log"
+    "mime"
+    "mime/multipart"
+    "net/http"
+    "strings"
 
-	"github.com/mzzsml/nparse"
+    "github.com/mzzsml/nparse"
 
-	"github.com/mzzsml/minerva/internal/storage"
+    "github.com/mzzsml/minerva/internal/storage"
 )
 
 type Server struct {
-	Addr  string
-	Store *storage.DB
+    Addr  string
+    Store *storage.DB
 }
 
 func (s *Server) Start() {
-	mux := http.NewServeMux()
+    mux := http.NewServeMux()
 
-	mux.HandleFunc("/{$}", s.handleIndex)
-	mux.HandleFunc("POST /upload", s.handleUpload)
-	mux.HandleFunc("GET /hosts/{$}", s.handleGetHosts)
-	mux.HandleFunc("GET /hosts/{addr}", s.handleGetHostDetails)
+    mux.HandleFunc("/{$}", s.handleIndex)
+    mux.HandleFunc("POST /upload", s.handleUpload)
+    mux.HandleFunc("GET /hosts/{$}", s.handleGetHosts)
+    mux.HandleFunc("GET /hosts/{addr}", s.handleGetHostDetails)
+    mux.HandleFunc("DELETE /hosts/{addr}", s.handleDeleteHost)
 
-	server := &http.Server{
-		Addr:    s.Addr,
-		Handler: mux,
-	}
+    server := &http.Server{
+        Addr:    s.Addr,
+        Handler: mux,
+    }
 
-	log.Printf("minerva is running on %v", server.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+    log.Printf("minerva is running on %v", server.Addr)
+    if err := server.ListenAndServe(); err != nil {
+        log.Fatal(err)
+    }
 }
 
 func (s Server) handleIndex(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.New("upload").ParseFiles("./internal/template/upload.html"))
-	err := t.Execute(w, nil)
-	if err != nil {
-		log.Printf("error: %s\n", err)
-		return
-	}
+    t := template.Must(template.New("upload").ParseFiles("./internal/template/upload.html"))
+    err := t.Execute(w, nil)
+    if err != nil {
+        log.Printf("error: %s\n", err)
+        return
+    }
 }
 
 func (s Server) handleUpload(w http.ResponseWriter, r *http.Request) {
-	mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
-	if err != nil {
-		log.Printf("error: %s\n", err)
-		return
-	}
+    mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+    if err != nil {
+        log.Printf("error: %s\n", err)
+        return
+    }
 
-	if strings.HasPrefix(mediaType, "multipart/") {
-		mr := multipart.NewReader(r.Body, params["boundary"])
+    if strings.HasPrefix(mediaType, "multipart/") {
+        mr := multipart.NewReader(r.Body, params["boundary"])
 
-		for {
-			p, err := mr.NextPart()
-			if err == io.EOF {
-				return
-			}
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				log.Printf("error: %s\n", err)
-				return
-			}
-			slurp, err := io.ReadAll(p)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				log.Printf("error: %s\n", err)
-				return
-			}
-			nmapScan, _ := nparse.NewNmapScan(slurp)
-			err = s.Store.InsertHosts(nmapScan)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintf(w, "Error in inserting new host in db: %s\n", err)
-				return
-			} else {
-				// AS OF TODAY 03/12/25 IT RETURNS NO ERROR IF HOST ALREADY
-				// EXISTS, SO IT WILL PRINT UPLOADED SUCC... EVEN IF IT DIDNT
-				// ACTUALLY UPLOAD IT.
-				w.WriteHeader(http.StatusOK)
-				fmt.Fprintf(w, "Uploaded successfully.\n")
-			}
-		}
-	}
+        for {
+            p, err := mr.NextPart()
+            if err == io.EOF {
+                return
+            }
+            if err != nil {
+                w.WriteHeader(http.StatusInternalServerError)
+                log.Printf("error: %s\n", err)
+                return
+            }
+            slurp, err := io.ReadAll(p)
+            if err != nil {
+                w.WriteHeader(http.StatusInternalServerError)
+                log.Printf("error: %s\n", err)
+                return
+            }
+            nmapScan, _ := nparse.NewNmapScan(slurp)
+            err = s.Store.InsertHosts(nmapScan)
+            if err != nil {
+                w.WriteHeader(http.StatusInternalServerError)
+                fmt.Fprintf(w, "Error in inserting new host in db: %s\n", err)
+                return
+            } else {
+                // AS OF TODAY 03/12/25 IT RETURNS NO ERROR IF HOST ALREADY
+                // EXISTS, SO IT WILL PRINT UPLOADED SUCC... EVEN IF IT DIDNT
+                // ACTUALLY UPLOAD IT.
+                w.WriteHeader(http.StatusOK)
+                fmt.Fprintf(w, "Uploaded successfully.\n")
+            }
+        }
+    }
 }
 
 func (s Server) handleGetHosts(w http.ResponseWriter, r *http.Request) {
-	hosts, err := s.Store.GetHosts()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "error: %s\n", err)
-		return
-	}
-	m, _ := json.Marshal(hosts)
-	w.WriteHeader(http.StatusOK)
-	log.Printf("%s %s\n", r.Method, r.URL)
-	fmt.Fprintf(w, "%s\n", m)
+    hosts, err := s.Store.GetHosts()
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        fmt.Fprintf(w, "error: %s\n", err)
+        return
+    }
+    m, _ := json.Marshal(hosts)
+    w.WriteHeader(http.StatusOK)
+    log.Printf("%s %s\n", r.Method, r.URL)
+    fmt.Fprintf(w, "%s\n", m)
 }
 
 func (s *Server) handleGetHostDetails(w http.ResponseWriter, r *http.Request) {
-	host, err := s.Store.GetHostInfoByIPv4(r.PathValue("addr"))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "error: %s\n", err)
-		return
-	}
+    host, err := s.Store.GetHostInfoByIPv4(r.PathValue("addr"))
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        fmt.Fprintf(w, "error: %s\n", err)
+        return
+    }
 
-	h, _ := json.Marshal(host)
-	w.WriteHeader(http.StatusOK)
-	log.Printf("%s %s\n", r.Method, r.URL)
-	fmt.Fprintf(w, "%s\n", h)
+    h, _ := json.Marshal(host)
+    w.WriteHeader(http.StatusOK)
+    log.Printf("%s %s\n", r.Method, r.URL)
+    fmt.Fprintf(w, "%s\n", h)
+}
+
+func (s *Server) handleDeleteHost(w http.ResponseWriter, r *http.Request) {
+    if ok := s.Store.DeleteHost(r.PathValue("addr")); ok {
+        fmt.Fprintf(w, `{"status":"host deleted successully"}`)
+        log.Printf("%s %s\n", r.Method, r.URL)
+    }
+    w.WriteHeader(http.StatusNotModified)
+    return
 }
--- internal/storage/db.go
+++ internal/storage/db.go
@@ -1,14 +1,14 @@
 package storage
 
 import (
-	"context"
-	"fmt"
-	"log"
+    "context"
+    "fmt"
+    "log"
 
-	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
 
-	"github.com/mzzsml/nparse"
+    "github.com/mzzsml/nparse"
 )
 
 var ctx context.Context = context.Background()
@@ -16,29 +16,29 @@ var ctx context.Context = context.Background()
 // Cnnect() handles the authentication and connection to the Postgres db.
 // Returns a *pgxpool.Pool object.
 func NewConnectionPool(dbfile string) (*sql.DB, error) {
-	var dsn string
-	dsn = fmt.Sprintf("file:%s?mode=rw", dbfile)
-	pool, err := sql.Open("sqlite3", dsn)
-	if err != nil {
-		return nil, err
-	}
-	err = pool.Ping()
-	if err != nil {
-		return nil, err
-	}
-	return pool, nil
+    var dsn string
+    dsn = fmt.Sprintf("file:%s?mode=rw", dbfile)
+    pool, err := sql.Open("sqlite3", dsn)
+    if err != nil {
+        return nil, err
+    }
+    err = pool.Ping()
+    if err != nil {
+        return nil, err
+    }
+    return pool, nil
 }
 
 // Db holds the connecton pool.
 // Using this struct it's possible to query the db without the need of
 // reautentication every time.
 type DB struct {
-	Pool *sql.DB
+    Pool *sql.DB
 }
 
 // NewDb creates a new Db instance.
 func NewDb(pool *sql.DB) *DB {
-	return &DB{pool}
+    return &DB{pool}
 }
 
 // BEFORE INSERTING PORTS, CHECK WETHER THEY ALREADY EXIST
@@ -48,86 +48,86 @@ func (d DB) isPortAlreadyExisting() {}
 // host.
 // It takes the host id and a nparse.Port as input.
 func (d DB) InsertPorts(hostId int, p nparse.Port) {
-	q := `INSERT INTO
+    q := `INSERT INTO
             port (host_id, protocol, port_num, state, reason, service, product, version, extrainfo)
             values ($1, $2, $3, $4, $5, $6, $7, $8, $9);`
 
-	_ = d.Pool.QueryRowContext(
-		ctx,
-		q,
-		hostId,
-		p.Protocol,
-		p.PortId,
-		p.State.State,
-		p.State.Reason,
-		p.Service.Name,
-		p.Service.Product,
-		p.Service.Version,
-		p.Service.Extrainfo,
-	)
+    _ = d.Pool.QueryRowContext(
+        ctx,
+        q,
+        hostId,
+        p.Protocol,
+        p.PortId,
+        p.State.State,
+        p.State.Reason,
+        p.Service.Name,
+        p.Service.Product,
+        p.Service.Version,
+        p.Service.Extrainfo,
+    )
 }
 
 func (d DB) IsHostAlreadyExisting(ipv4 string) bool {
-	var exists bool
-	q := `SELECT EXISTS(SELECT id FROM hosts WHERE ipv4 = $1);`
-	err := d.Pool.QueryRowContext(ctx, q, ipv4).Scan(&exists)
-	if err == nil && exists {
-		return true
-	}
-	return false
+    var exists bool
+    q := `SELECT EXISTS(SELECT id FROM hosts WHERE ipv4 = $1);`
+    err := d.Pool.QueryRowContext(ctx, q, ipv4).Scan(&exists)
+    if err == nil && exists {
+        return true
+    }
+    return false
 }
 
 // InsertHosts inserts a new host in the database, if the host does not already
 // exists.
 // NOTE: as of now, InsertHosts does NOT insert the host's hostname into the db.
 func (d DB) InsertHosts(n *nparse.NmapScan) error {
-	var id int
-	//q := `INSERT INTO hosts (ipv4, mac, vendor, hostname) VALUES ($1, $2, $3, $4) RETURNING id;`
-	q := `INSERT INTO hosts (ipv4, mac, vendor) VALUES ($1, $2, $3) RETURNING id;`
-	for _, h := range n.Hosts {
-		tmphost := make(map[string]string)
-		for _, a := range h.Addrs {
-			if a.AddrType == "ipv4" {
-				tmphost["ipv4"] = a.Addr
-				continue
-			}
-			if a.AddrType == "mac" {
-				tmphost["mac"] = a.Addr
-				tmphost["vendor"] = a.Vendor
-				continue
-			}
-		}
-		if !d.IsHostAlreadyExisting(tmphost["ipv4"]) {
-			err := d.Pool.QueryRowContext(
-				ctx,
-				q,
-				tmphost["ipv4"],
-				tmphost["mac"],
-				tmphost["vendor"],
-			).Scan(&id)
-			if err != nil {
-				return err
-			}
-			for _, p := range h.Ports {
-				d.InsertPorts(id, p)
-			}
-		} else {
-			// TODO: ERROR HOST ALREADY EXISTS.
-			return nil
-		}
-	}
-	return nil
+    var id int
+    //q := `INSERT INTO hosts (ipv4, mac, vendor, hostname) VALUES ($1, $2, $3, $4) RETURNING id;`
+    q := `INSERT INTO hosts (ipv4, mac, vendor) VALUES ($1, $2, $3) RETURNING id;`
+    for _, h := range n.Hosts {
+        tmphost := make(map[string]string)
+        for _, a := range h.Addrs {
+            if a.AddrType == "ipv4" {
+                tmphost["ipv4"] = a.Addr
+                continue
+            }
+            if a.AddrType == "mac" {
+                tmphost["mac"] = a.Addr
+                tmphost["vendor"] = a.Vendor
+                continue
+            }
+        }
+        if !d.IsHostAlreadyExisting(tmphost["ipv4"]) {
+            err := d.Pool.QueryRowContext(
+                ctx,
+                q,
+                tmphost["ipv4"],
+                tmphost["mac"],
+                tmphost["vendor"],
+            ).Scan(&id)
+            if err != nil {
+                return err
+            }
+            for _, p := range h.Ports {
+                d.InsertPorts(id, p)
+            }
+        } else {
+            // TODO: ERROR HOST ALREADY EXISTS.
+            return nil
+        }
+    }
+    return nil
 }
 
 // GetPorts queries the db and gets every port for a provided host.
 // It takes the host ID as input and returns and array of nmap.Port.
 func (d DB) GetPortsByHostId(hostId int) []nparse.Port {
-	var (
-		p     nparse.Port
-		ports []nparse.Port
-	)
+    var (
+        p     nparse.Port
+        ports []nparse.Port
+    )
 
-	q := `SELECT
+    q := `SELECT
             protocol,
             port_num,
             state,
@@ -139,87 +139,98 @@ func (d DB) GetPortsByHostId(hostId int) []nparse.Port {
             ports
           WHERE host_id = $1;`
 
-	rows, err := d.Pool.Query(q, hostId)
-	if err != nil {
-		log.Printf("error in reading ports from DB: %s\n", err)
-	}
-	defer rows.Close()
+    rows, err := d.Pool.Query(q, hostId)
+    if err != nil {
+        log.Printf("error in reading ports from DB: %s\n", err)
+    }
+    defer rows.Close()
 
-	for rows.Next() {
-		err := rows.Scan(
-			&p.Protocol,
-			&p.PortId,
-			&p.State.State,
-			&p.State.Reason,
-			&p.Service.Name,
-			&p.Service.Version,
-			&p.Service.Extrainfo,
-		)
-		if err != nil {
-			log.Printf("errror: %s\n", err)
-		}
-		ports = append(ports, p)
-	}
-	return ports
+    for rows.Next() {
+        err := rows.Scan(
+            &p.Protocol,
+            &p.PortId,
+            &p.State.State,
+            &p.State.Reason,
+            &p.Service.Name,
+            &p.Service.Version,
+            &p.Service.Extrainfo,
+        )
+        if err != nil {
+            log.Printf("errror: %s\n", err)
+        }
+        ports = append(ports, p)
+    }
+    return ports
 }
 
 type host struct {
-	Id   int    `json:"id"`
-	Ipv4 string `json:"ipv4"`
+    Id   int    `json:"id"`
+    Ipv4 string `json:"ipv4"`
 }
 
 // GetHosts queries the `host` table and returns a list of Host (which is
 // different than nparse.Host, and it just holds the hosts' ids and ipv4 addresses).
 func (d DB) GetHosts() ([]host, error) {
-	var h host
-	var hosts []host
+    var h host
+    var hosts []host
 
-	q := `SELECT id, ipv4 FROM hosts;`
-	rows, err := d.Pool.Query(q)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		err = rows.Scan(&h.Id, &h.Ipv4)
-		if err != nil {
-			return nil, err
-		}
-		hosts = append(hosts, h)
-	}
-	return hosts, nil
+    q := `SELECT id, ipv4 FROM hosts;`
+    rows, err := d.Pool.Query(q)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+    for rows.Next() {
+        err = rows.Scan(&h.Id, &h.Ipv4)
+        if err != nil {
+            return nil, err
+        }
+        hosts = append(hosts, h)
+    }
+    return hosts, nil
 }
 
 func (d DB) GetHostInfoByIPv4(addr string) (nparse.Host, error) {
-	var (
-		h        nparse.Host
-		ports    []nparse.Port
-		id       int
-		mac      string
-		vendor   string
-		hostname string
-	)
-	q := `SELECT id, mac, vendor, hostname FROM hosts WHERE ipv4=$1;`
-	err := d.Pool.QueryRowContext(ctx, q, addr).Scan(&id, &mac, &vendor, &hostname)
-	if err != nil {
-		return h, err
-	}
-	ports = d.GetPortsByHostId(id)
-	addrIPv4 := nparse.Address{
-		Addr:     addr,
-		AddrType: "ipv4",
-	}
-	addrMac := nparse.Address{
-		Addr:     mac,
-		AddrType: "mac",
-		Vendor:   vendor,
-	}
-	addrs := []nparse.Address{addrIPv4, addrMac}
-	hostnames := []string{hostname}
-	h = nparse.Host{
-		Addrs:     addrs,
-		Hostnames: hostnames,
-		Ports:     ports,
-	}
-	return h, nil
+    var (
+        h        nparse.Host
+        ports    []nparse.Port
+        id       int
+        mac      string
+        vendor   string
+        hostname string
+    )
+    q := `SELECT id, mac, vendor, hostname FROM hosts WHERE ipv4=$1;`
+    err := d.Pool.QueryRowContext(ctx, q, addr).Scan(&id, &mac, &vendor, &hostname)
+    if err != nil {
+        return h, err
+    }
+    ports = d.GetPortsByHostId(id)
+    addrIPv4 := nparse.Address{
+        Addr:     addr,
+        AddrType: "ipv4",
+    }
+    addrMac := nparse.Address{
+        Addr:     mac,
+        AddrType: "mac",
+        Vendor:   vendor,
+    }
+    addrs := []nparse.Address{addrIPv4, addrMac}
+    hostnames := []string{hostname}
+    h = nparse.Host{
+        Addrs:     addrs,
+        Hostnames: hostnames,
+        Ports:     ports,
+    }
+    return h, nil
+}
+
+func (d DB) DeleteHost(addr string) bool {
+    q := `DELETE FROM hosts WHERE ipv4 = $1;`
+    if !d.IsHostAlreadyExisting(addr) {
+        return false
+    }
+    if _, err := d.Pool.ExecContext(ctx, q, addr); err != nil {
+        return false
+    }
+    return true
 }
